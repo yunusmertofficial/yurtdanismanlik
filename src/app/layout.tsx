daf3ae34ec24462b3cda4ef1ea32447686700006
Eminmert30 <yunusmertofficial@gmail.com>
2025-02-15 00:12:15 +0300

    Add @next/third-parties dependency and integrate Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import WhatsAppButton from "@/components/WhatsappButton";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <GoogleAnalytics gaId="G-QX3S4JJJVM" />
         <Header />
         {children}
         <Footer />
